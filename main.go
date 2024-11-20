013fd7dd985b5d06a7a59b2659ebb74e1e401cab
Hayden Durham <hayden.t.durham@gmail.com>
2024-11-19 19:39:50 -0600

    Continued with the add task functionality

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand/v2"
+	"os"
+	"strconv"
 )
 
 var (
@@ -14,14 +18,65 @@ var (
 
 func main() {
 	fmt.Println("Silly To-Do App!")
-	flag.IntVar(&idFlag, "id", 0, "id of task")
+	flag.IntVar(&idFlag, "id", -1, "id of task")
 	flag.StringVar(&addFlag, "a", "", "adds task")
 	flag.StringVar(&deleteFlag, "d", "", "deletes task")
 	flag.StringVar(&statusFlag, "s", "", "outputs all tasks")
 	flag.Parse()
 
-	fmt.Println("idFlag value is: ", idFlag)
-	fmt.Println("addFlag value is: ", addFlag)
-	fmt.Println("deletFlag value is: ", deleteFlag)
-	fmt.Println("statusFlag value is: ", statusFlag)
+	if addFlag != "" {
+		add(addFlag, "output.csv")
+	} else if deleteFlag != "" && idFlag != -1 {
+		delete(idFlag)
+	} else if statusFlag != "" {
+		status()
+	} else {
+		fmt.Println("Invalid argument")
+	}
+
+}
+
+// Will add the task
+func add(task string, filename string) {
+	taskID := strconv.Itoa(rand.IntN(15))
+	writer, file, err := createCSVWriter(filename)
+	if err != nil {
+		fmt.Println("Error creating CSV writer: ", err)
+		return
+	}
+	defer file.Close()
+	header := []string{"ID", "Task"}
+	writeCSVRecord(writer, header)
+	record := []string{taskID, task}
+	writeCSVRecord(writer, record)
+
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		fmt.Println("Error flushing CSV writer: ", err)
+	}
+}
+
+// Given a task ID, will delete that task
+func delete(taskID int) {
+
+}
+
+// Will print all tasks that are still needing to be complete
+func printTasks() {}
+
+// Creates CSV File
+func createCSVWriter(filename string) (*csv.Writer, *os.File, error) {
+	f, err := os.Create(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	writer := csv.NewWriter(f)
+	return writer, f, nil
+}
+
+func writeCSVRecord(writer *csv.Writer, record []string) {
+	err := writer.Write(record)
+	if err != nil {
+		fmt.Println("Error writing record to CSV: ", err)
+	}
 }
